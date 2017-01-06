818c3253289c483235033f112584f8348b3ba64b
Mingyu Kim <dpcm@naver.com>
2017-01-06 10:32:00 +0900

    Add Chat_Server
    채팅 서버

---
+++ 06_게임서버프로그래밍/Chat_Server/Chat_Server/ProtocolBuffer.h
@@ -0,0 +1,183 @@
+/*---------------------------------------------------------------
+네트워크 패킷용 클래스.
+간편하게 패킷에 순서대로 데이타를 In, Out 한다.
+
+- 사용법.
+CAyaPacket cPacket;
+넣기.
+clPacket << 40030;	or	clPacket << iValue;	(int 넣기)
+clPacket << 3;		or	clPacket << byValue;(BYTE 넣기)
+clPacket << 1.4;	or	clPacket << fValue;	(float 넣기)
+
+빼기.
+clPacket >> iValue;	(int 빼기)
+clPacket >> byValue;(BYTE 빼기)
+clPacket >> fValue;	(float 빼기)
+
+!.	삽입되는 데이타 FIFO 순서로 관리된다.
+큐가 아니므로, 넣기(<<).빼기(>>) 를 혼합해서 사용하면 안된다.
+----------------------------------------------------------------*/
+
+#ifndef  __PROTOCOL_BUFFER_H__
+#define  __PROTOCOL_BUFFER_H__
+
+#include <Windows.h>
+
+class CProtocolBuffer
+{
+public:
+	/*---------------------------------------------------------------
+	Enum.
+	----------------------------------------------------------------*/
+
+	enum enProtocolBuffer
+	{
+		eBUFFER_DEFAULT = 512		// 패킷의 기본 버퍼 사이즈.
+	};
+
+	//////////////////////////////////////////////////////////////////////////
+	// 생성자, 파괴자.
+	//
+	// Return:
+	//////////////////////////////////////////////////////////////////////////
+	CProtocolBuffer();
+	CProtocolBuffer(int iBufferSize);
+
+	virtual	~CProtocolBuffer();
+
+	//////////////////////////////////////////////////////////////////////////
+	// 패킷 초기화.
+	//
+	// 메모리 할당을 여기서 하므로, 함부로 호출하면 안된다. 
+	//
+	// Parameters: (int)BufferSize.
+	// Return: 없음.
+	//////////////////////////////////////////////////////////////////////////
+	void	Initial(int iBufferSize = eBUFFER_DEFAULT);
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// 패킷  파괴.
+	//
+	// Parameters: 없음.
+	// Return: 없음.
+	//////////////////////////////////////////////////////////////////////////
+	void	Release(void);
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// 패킷 청소.
+	//
+	// Parameters: 없음.
+	// Return: 없음.
+	//////////////////////////////////////////////////////////////////////////
+	void	Clear(void);
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// 버퍼 사이즈 얻기.
+	//
+	// Parameters: 없음.
+	// Return: (int)패킷 버퍼 사이즈 얻기.
+	//////////////////////////////////////////////////////////////////////////
+	int		GetBufferSize(void) { return m_iBufferSize; }
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// 현재 사용중인 사이즈 얻기.
+	//
+	// Parameters: 없음.
+	// Return: (int)사용중인 데이타 사이즈.
+	//////////////////////////////////////////////////////////////////////////
+	int		GetDataSize(void) { return m_iDataSize; }
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// 버퍼 포인터 얻기.
+	//
+	// Parameters: 없음.
+	// Return: (char *)버퍼 포인터.
+	//////////////////////////////////////////////////////////////////////////
+	char	*GetBufferPtr(void);
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// 버퍼 Pos 이동. (음수이동은 안됨)
+	// GetBufferPtr 함수를 이용하여 외부에서 강제로 버퍼 내용을 수정할 경우 사용. 
+	//
+	// Parameters: (int) 이동 사이즈.
+	// Return: (int) 이동된 사이즈.
+	//////////////////////////////////////////////////////////////////////////
+	int		MoveWritePos(int iSize);
+	int		MoveReadPos(int iSize);
+
+	/* ============================================================================= */
+	// 연산자 오퍼레이터.
+	/* ============================================================================= */
+
+	CProtocolBuffer	&operator = (CProtocolBuffer &clSrcAyaPacket);
+	//////////////////////////////////////////////////////////////////////////
+	// 넣기.	각 변수 타입마다 모두 만듬.
+	//////////////////////////////////////////////////////////////////////////
+	CProtocolBuffer	&operator << (BYTE byValue);
+	CProtocolBuffer	&operator << (char chValue);
+	CProtocolBuffer	&operator << (short shValue);
+	CProtocolBuffer	&operator << (WORD wValue);
+	CProtocolBuffer	&operator << (int iValue);
+	CProtocolBuffer	&operator << (DWORD dwValue);
+	CProtocolBuffer	&operator << (float fValue);
+	CProtocolBuffer	&operator << (__int64 iValue);
+	CProtocolBuffer	&operator << (double dValue);
+
+
+	//////////////////////////////////////////////////////////////////////////
+	// 빼기.	각 변수 타입마다 모두 만듬.
+	//////////////////////////////////////////////////////////////////////////
+	CProtocolBuffer	&operator >> (BYTE &byValue);
+	CProtocolBuffer	&operator >> (char &chValue);
+	CProtocolBuffer	&operator >> (short &shValue);
+	CProtocolBuffer	&operator >> (WORD &wValue);
+	CProtocolBuffer	&operator >> (int &iValue);
+	CProtocolBuffer	&operator >> (DWORD &dwValue);
+	CProtocolBuffer	&operator >> (float &fValue);
+	CProtocolBuffer	&operator >> (__int64 &iValue);
+	CProtocolBuffer	&operator >> (double &dValue);
+
+	//////////////////////////////////////////////////////////////////////////
+	// 데이타 얻기.
+	//
+	// Parameters: (char *)Dest 포인터. (int)Size.
+	// Return: (int)복사한 사이즈.
+	//////////////////////////////////////////////////////////////////////////
+	int		GetData(char *chpDest, int iSize);
+
+	//////////////////////////////////////////////////////////////////////////
+	// 데이타 삽입.
+	//
+	// Parameters: (char *)Src 포인터. (int)SrcSize.
+	// Return: (int)복사한 사이즈.
+	//////////////////////////////////////////////////////////////////////////
+	int		PutData(char *chpSrc, int iSrcSize);
+
+protected:
+	//------------------------------------------------------------
+	// 패킷버퍼 / 버퍼 사이즈.
+	//------------------------------------------------------------
+	char	m_chaBufferDefault[eBUFFER_DEFAULT];
+	char	*m_chpBuffer;		// 필요한지
+	int		m_iBufferSize;
+
+	//------------------------------------------------------------
+	// 버퍼의 읽을 위치, 넣을 위치.
+	//------------------------------------------------------------
+	char	*m_chpReadPos;
+	char	*m_chpWritePos;
+
+	//------------------------------------------------------------
+	// 현재 버퍼에 사용중인 사이즈.
+	//------------------------------------------------------------
+	int		m_iDataSize;	// m_chpWritePos - m_chpReadPos ?
+							// m_chpWritePos ?
+};
+
+#endif // !__PROTOCOL_BUFFER_H__
---
+++ 06_게임서버프로그래밍/Chat_Server/Chat_Server/main.cpp
@@ -0,0 +1,808 @@
+#pragma comment(lib, "ws2_32")
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <locale.h>
+#include <stdio.h>
+#include <list>
+#include <map>
+#include "Protocol.h"
+#include "ProtocolBuffer.h"
+#include "StreamSQ.h"
+
+using namespace std;
+
+// 클라이언트 구조체
+struct st_CLIENT
+{
+	SOCKET			sock;
+	SOCKADDR_IN		connectAddr;
+
+	CStreamSQ		SendQ;
+	CStreamSQ		RecvQ;
+
+	DWORD			dwUserNO;
+	DWORD			dwEnterRoomNO;
+
+	WCHAR			szNickName[dfNICK_MAX_LEN];
+};
+
+// 채팅방 구조체
+struct st_CHAT_ROOM
+{
+	DWORD			dwRoomNO;
+	WCHAR			szTitle[256];
+	list<DWORD>		UserList;
+};
+
+// 전역
+DWORD		g_dwKey_UserNO;					// User 아이디
+DWORD		g_dwKey_RoomNO;					// 채팅방
+
+map<DWORD, st_CLIENT*>		g_ClientMap;	// 사용자 관리 메인 map
+map<DWORD, st_CHAT_ROOM*>	g_RoomMap;		// 채팅방 관리 메인 map
+
+SOCKET		g_ListenSocket;					// 사용자 accept용 listen 소켓
+
+// 네트워크 초기화
+
+BOOL	NewworkInitial(void);
+
+void	NetworkProcess(void);
+
+void	SelectSocket(DWORD *dwpTableNO, SOCKET *pTableSocket, 
+	FD_SET *pReadSet, FD_SET *pWriteSet);
+
+int		MakeRecvPacket(st_CLIENT *pClient);
+
+BYTE	MakeCheckSum(CProtocolBuffer *pPacket, WORD wMsgType);
+//
+BOOL	PacketProc(st_CLIENT *pClient, WORD wMsgType, CProtocolBuffer *pPacket);
+//
+//
+// netproc
+void	netProc_Recv(DWORD dwUserNO);
+void	netProc_Send(DWORD dwUserNO);
+void	netProc_Accept(void);
+
+// Client 찾기
+st_CLIENT	*FindClient(DWORD dwUserNO);
+
+// Client 끊기
+BOOL	DisconnectClient(DWORD dwUserNO);
+
+
+BOOL	netPacket_ReqLogin(st_CLIENT *pClient, CProtocolBuffer *pPacket);
+BOOL	netPacket_ReqRoomList(st_CLIENT *pClient, CProtocolBuffer *pPacket);
+BOOL	netPacket_ReqRoomCreate(st_CLIENT *pClient, CProtocolBuffer *pPacket);
+BOOL	netPacket_ReqRoomEnter(st_CLIENT *pClient, CProtocolBuffer *pPacket);
+BOOL	netPacket_ReqChat(st_CLIENT *pClient, CProtocolBuffer *pPacket);
+BOOL	netPacket_ReqRoomLeave(st_CLIENT *pClient, CProtocolBuffer *pPacket);
+
+
+void	Send_ResLogin(st_CLIENT *pClient, BYTE byResult);
+void	Send_ResRoomList(st_CLIENT *pClient, BYTE byResult);
+void	Send_ResRoomCreate(st_CLIENT *pClient, st_CHAT_ROOM *pRoom, BYTE byResult);
+void	Send_ResRoomEnter(st_CLIENT *pClient, st_CHAT_ROOM *pRoom, BYTE byResult);
+void	Send_ResChat(st_CLIENT *pClient, WORD wMsgSize, PWCHAR szMsg);
+void	Send_ResRoomLeave(st_CLIENT *pClient);
+void	Send_ResRoomDelete(st_CLIENT *pClient, st_CHAT_ROOM *pRoom);
+void	Send_ResUserEnter(st_CLIENT *pClient);
+
+
+void	MakePacket_ResLogin(st_PACKET_HEADER *pHeader, CProtocolBuffer *pPacket, BYTE byResult, st_CLIENT *pClient);
+void	MakePacket_ResRoomList(st_PACKET_HEADER *pHeader, CProtocolBuffer *pPacket, BYTE byResult);
+void	MakePacket_ResRoomCreate(st_PACKET_HEADER *pHeader, CProtocolBuffer *pPacket, BYTE byResult, st_CHAT_ROOM *pRoom);
+void	MakePacket_ResRoomEnter(st_PACKET_HEADER *pHeader, CProtocolBuffer *pPacket, BYTE byResult, st_CHAT_ROOM *pRoom);
+void	MakePacket_ResChat(st_PACKET_HEADER *pHeader, CProtocolBuffer *pPacket, st_CLIENT *pClient, BYTE byResult, WORD wMsgSize, PWCHAR szMsg);
+void	MakePacket_ResUserEnter(st_PACKET_HEADER *pHeader, CProtocolBuffer *pPacket, st_CLIENT *pClient);
+void	MakePacket_ResRoomLeave(st_PACKET_HEADER *pHeader, CProtocolBuffer *pPacket, st_CLIENT *pClient);
+void	MakePacket_ResRoomDelete(st_PACKET_HEADER *pHeader, CProtocolBuffer *pPacket, st_CLIENT *pClient, st_CHAT_ROOM *pRoom);
+
+// 특정 클라이언트에게 보내기
+void	SendUnicast(st_CLIENT *pClient, st_PACKET_HEADER *pHeader, CProtocolBuffer *pPacket);
+
+// 모든 유저에게 보내기
+void	SendBroadcast(st_PACKET_HEADER *pHeader, CProtocolBuffer *pPacket);
+
+// 방 유저에게 보내기( 당사자는 제외)
+void	SendBroadcastRoom(st_CHAT_ROOM *pRoom, st_CLIENT *pClient, 
+	st_PACKET_HEADER *pHeader, CProtocolBuffer *pPacket);
+
+// packet을 받아서 checksum 만들기
+BYTE	MakeCheckSum(CProtocolBuffer *pPacket, WORD dwType);
+
+
+void err_quit(WCHAR *msg);
+void err_display(WCHAR *msg);
+
+int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
+{
+	setlocale(LC_ALL, "");	// 유니코드 스탠다드 출력 환경 세팅
+
+	if (!NewworkInitial())
+	{
+		wprintf(L"NewworkInitial()");
+		return 0;
+	}
+
+
+	while (1)
+	{
+		NetworkProcess();
+
+		// 특정 키 및 상황에 따른 종료처리
+		
+	}
+
+	// 서버 마무리 종료 작업
+	return 0;
+}
+
+// 소켓 입출력 절차
+// 1. 소켓 셋을 비운다
+// 2. 소켓 셋에 소켓을 넣는다
+// 3. select 함수를 호출한다
+// 4. select 함수가 리턴하면적절한 소켓함수를 호출하여 처리한다
+
+BOOL NewworkInitial(void)
+{
+	// 윈속 초기화
+	WSADATA wsa;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		return FALSE;
+	}
+
+	// socket()
+	g_ListenSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (g_ListenSocket == INVALID_SOCKET)
+	{
+		err_display(L"socket()");
+		return FALSE;
+	}
+
+	// bind()
+	SOCKADDR_IN serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family			= AF_INET;
+	serveraddr.sin_addr.S_un.S_addr	= htonl(INADDR_ANY);
+	serveraddr.sin_port				= htons(dfNETWORK_PORT);
+
+	int retval = bind(g_ListenSocket, (SOCKADDR*)&serveraddr, sizeof(serveraddr));
+
+	if (retval == SOCKET_ERROR)
+	{
+		err_display(L"bind()");
+		return FALSE;
+	}
+
+	// listen()
+	retval = listen(g_ListenSocket, SOMAXCONN);
+
+	if (retval == SOCKET_ERROR)
+	{
+		err_display(L"listen()");
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+void NetworkProcess(void)
+{
+	st_CLIENT *pClient;
+	
+	DWORD	userTable_NO[FD_SETSIZE]	 = { -1, };
+	SOCKET	userTable_SOCKET[FD_SETSIZE] = { INVALID_SOCKET };
+
+	int		iSocketCount = 0;	// 64명 되면 호출
+
+	FD_SET	readSet;
+	FD_SET	writeSet;
+
+	FD_ZERO(&readSet);
+	FD_ZERO(&writeSet);
+
+	memset(userTable_NO, -1, FD_SETSIZE);
+	memset(userTable_SOCKET, INVALID_SOCKET, FD_SETSIZE);
+
+
+	// 첫 번째 인덱스에는 listen socket 넣을 것임
+	FD_SET(g_ListenSocket, &readSet);
+	userTable_NO[iSocketCount] = 0;
+	userTable_SOCKET[iSocketCount] = g_ListenSocket;
+
+	iSocketCount++;
+
+
+	// Client를 순회하면서 처리
+	map<DWORD, st_CLIENT*>::iterator iter;
+
+	for (iter = g_ClientMap.begin(); iter != g_ClientMap.end(); ++iter)
+	{
+		pClient				= (*iter).second;
+		userTable_NO[iSocketCount]		= pClient->dwUserNO;
+		userTable_SOCKET[iSocketCount]	= pClient->sock;
+
+		FD_SET(pClient->sock, &readSet);
+		FD_SET(pClient->sock, &writeSet);
+
+		iSocketCount++;
+
+		if (iSocketCount >= FD_SETSIZE)
+		{
+			// FD_ISSET으로 recv send 처리
+			SelectSocket(userTable_NO, userTable_SOCKET, &readSet, &writeSet);
+
+			FD_ZERO(&readSet);
+			FD_ZERO(&writeSet);
+
+			memset(userTable_NO, -1, FD_SETSIZE);
+			memset(userTable_SOCKET, INVALID_SOCKET, FD_SETSIZE);
+
+			iSocketCount = 0;
+		}
+	}
+	
+
+	// 마지막에 iSocketCount가 FD_SETSIZE만큼
+	// 도달하지 못했을 경우에는 다시 한번 처리해줌
+	if(0 < iSocketCount )
+	{
+		SelectSocket(userTable_NO, userTable_SOCKET, &readSet, &writeSet);
+	}
+	
+}
+
+void SelectSocket(DWORD * dwpTable, SOCKET * pSocketTable, 
+					FD_SET *pReadSet, FD_SET *pWriteSet)
+{
+	timeval		time;
+	int			iResult;
+
+	// 64명씩 돌면서 하기 때문에 시간을 넣으면 안 됨
+	time.tv_sec		= 0;
+	time.tv_usec	= 0;
+
+	iResult = select(0, pReadSet, pWriteSet, NULL, &time);
+
+	// iResult 값이 0 이상이면
+	// 64명 테이블을 돌면서 FD_ISSET으로 readSet과 writeSet 검사
+	if (0 < iResult)
+	{
+		for (int iCount = 0; iCount < FD_SETSIZE; iCount++)
+		{
+			if (INVALID_SOCKET == pSocketTable[iCount])
+			{
+				continue;
+			}
+
+			if (FD_ISSET(pSocketTable[iCount], pReadSet))
+			{
+				if (iCount == 0)
+				{
+					netProc_Accept();
+				}
+				else
+				{
+					netProc_Recv(dwpTable[iCount]);
+				}
+			}
+
+			if (FD_ISSET(pSocketTable[iCount], pWriteSet))
+			{
+				netProc_Send(dwpTable[iCount]);
+			}
+		}
+	}
+	else if (INVALID_SOCKET == iResult)
+	{
+		err_quit(L"SelectSocket()");
+	}
+}
+
+int MakeRecvPacket(st_CLIENT * pClient)
+{
+	// return 1		패킷 미 완성
+	// return 0		패킷 처리
+	// return -1	오류
+	
+	st_PACKET_HEADER	stHeader;
+
+	int iRecvQSize = pClient->RecvQ.GetUseSize();
+
+	// 지금 recvQ에 들어있는게 헤더보다 작으면 처리할 게 없음
+	if (sizeof(st_PACKET_HEADER) > iRecvQSize)
+	{
+		return 1;
+	}
+
+	// 패킷 코드 검사
+	pClient->RecvQ.Peek( (char*)&stHeader, sizeof(st_PACKET_HEADER) );
+
+	if (dfPACKET_CODE != stHeader.byCode)
+	{
+		return -1;
+	}
+
+	// 헤더 + 페이로드보다 recvQ 크기가 작으면 미완성
+	if (stHeader.wPayloadSize + sizeof(stHeader) > iRecvQSize)
+	{
+		return 1;
+	}
+
+	// 헤더부분은 빼버리고
+	// payload만 가져옴
+	pClient->RecvQ.RemoveData(	sizeof( stHeader )	);
+
+	CProtocolBuffer clPacket;
+
+	if (	stHeader.wPayloadSize != pClient->RecvQ.Get(clPacket.GetBufferPtr(), stHeader.wPayloadSize	)	)
+	{
+		return -1;
+	}
+
+	pClient->RecvQ.RemoveData(stHeader.wPayloadSize);
+
+	// 체크섬 확인
+	BYTE byChecksum = MakeCheckSum(&clPacket, stHeader.wMsgType);
+
+	if (byChecksum != stHeader.wPayloadSize)
+	{
+		err_display(L"CheckSum");
+
+		return -1;
+	}
+
+	// 실질적인 패킷 처리 함수 호출
+	if ( !PacketProc(pClient, stHeader.wMsgType, &clPacket)	)
+	{
+		return -1;
+	}
+
+	return 0;
+}
+
+BYTE MakeCheckSum(CProtocolBuffer * pPacket, WORD wMsgType)
+{
+	BYTE		byCheckSum	= 0;
+	int			iPacketSize = pPacket->GetDataSize();
+	BYTE		*bypTemp	= (BYTE*)pPacket->GetBufferPtr();
+
+	//byCheckSum	= LOBYTE(wMsgType) + HIBYTE(wMsgType);
+	byCheckSum = wMsgType;
+
+	for (int iCount = 0; iCount < iPacketSize; iCount++)
+	{
+		byCheckSum += bypTemp[iCount];
+	}
+
+	byCheckSum %= 256;
+	
+	return byCheckSum;
+}
+
+BOOL PacketProc(st_CLIENT * pClient, WORD wMsgType, CProtocolBuffer * pPacket)
+{
+	wprintf(L"PacketRecv [UserNO:%d][Type:%d]\n", pClient->dwUserNO, wMsgType);
+	
+	switch (wMsgType)
+	{
+	case df_REQ_LOGIN:
+		return netPacket_ReqLogin(pClient, pPacket);
+
+	case df_REQ_ROOM_LIST:
+		return netPacket_ReqRoomList(pClient, pPacket);
+
+	case df_REQ_ROOM_CREATE:
+		return netPacket_ReqRoomCreate(pClient, pPacket);
+
+	case df_REQ_ROOM_ENTER:
+		return netPacket_ReqRoomEnter(pClient, pPacket);
+	case df_REQ_CHAT:
+		return netPacket_ReqRoomEnter(pClient, pPacket);
+
+	case df_REQ_ROOM_LEAVE:
+		return netPacket_ReqRoomLeave(pClient, pPacket);
+	}
+	return TRUE;
+}
+
+
+
+void err_quit(WCHAR * msg)
+{
+	LPVOID lpMsgBuf;
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+void err_display(WCHAR * msg)
+{
+	LPVOID lpMsgBuf;
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+
+	wprintf(L"[%s] %s", msg, (WCHAR *)lpMsgBuf);
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+void netProc_Recv(DWORD dwUserNO)
+{
+	st_CLIENT *pClient = g_ClientMap[dwUserNO];
+	int		iResult = 0;
+
+
+	if (nullptr == pClient)
+	{
+		return;
+	}
+
+	iResult = recv(pClient->sock, pClient->RecvQ.GetWriteBufferPtr(), 
+				pClient->RecvQ.GetFreeSize(), 0);
+
+	if (SOCKET_ERROR == iResult || 0 == iResult)
+	{
+		closesocket(pClient->sock);
+		DisconnectClient(dwUserNO);
+
+		return;
+	}
+	
+	if (0 < iResult)
+	{
+		// write한만큼 이동
+		pClient->RecvQ.MoveWritePos(iResult);
+
+		// 패킷이 하나 이상 있을 수 있으므로
+		// 한 번에 전부 처리할 것
+		while (1)
+		{
+			iResult = MakeRecvPacket(pClient);
+
+			if (1 == iResult)
+			{
+				break;
+			}
+
+			if (-1 == iResult)
+			{
+				err_display(L"MakeRecvPacket()");
+				return;
+			}
+		}
+	}
+}
+
+void netProc_Send(DWORD dwUserNO)
+{
+	st_CLIENT *pClient;
+
+	int			iResult;
+	int			iSendSize;
+
+	pClient = g_ClientMap[dwUserNO];
+
+	if (nullptr == pClient)
+	{
+		return;
+	}
+
+	iSendSize = pClient->SendQ.GetUseSize();
+
+	if (0 >= iSendSize)
+	{
+		return;
+	}
+
+	iResult = send(pClient->sock, pClient->SendQ.GetReadBufferPtr(), iSendSize, 0);
+
+	if (SOCKET_ERROR == iResult)
+	{
+		DWORD dwError = WSAGetLastError();
+
+		if (WSAEWOULDBLOCK == dwError)
+		{
+			wprintf(L"Socket WOULDBLOCK - UserNO:%d\n", dwUserNO);
+			return;
+		}
+
+		wprintf(L"Socket Error - UserNO:%d ErrorCode:%d\n", dwUserNO, dwError);
+
+		closesocket(pClient->sock);
+		DisconnectClient(dwUserNO);
+		return;
+	}
+	else
+	{
+		if (iSendSize < iResult)
+		{
+			wprintf(L"Send size error - UserNO:%d SendSize:%d SendResult:%d\n",
+				dwUserNO, iSendSize, iResult);
+			return;
+		}
+		else
+		{
+			pClient->SendQ.RemoveData(iResult);
+		}
+	}
+}
+
+void netProc_Accept(void)
+{
+	st_CLIENT *pClient	= new st_CLIENT;
+	int	iAddrLen		= 0;
+	WCHAR szTemp[16] = { 0, };
+
+
+	pClient->dwEnterRoomNO	= -1;
+	pClient->dwUserNO		= g_dwKey_UserNO;
+	g_dwKey_UserNO++;
+	pClient->szNickName[0]	= NULL;
+
+	iAddrLen = sizeof(pClient->connectAddr);
+
+	pClient->sock = accept(g_ListenSocket, (SOCKADDR*)&(pClient->connectAddr), &iAddrLen);
+
+	if (INVALID_SOCKET == pClient->sock)
+	{
+		int ierr = WSAGetLastError();
+
+		if (WSAEWOULDBLOCK != ierr)
+		{
+			err_display(L"accept()");
+		}
+	}
+	else
+	{
+		g_ClientMap[g_dwKey_UserNO] = pClient;
+
+		InetNtop(AF_INET, &pClient->connectAddr.sin_addr, szTemp, 16);
+
+		wprintf(L"Accetp - %s:%d [UserNO:%d]\n",
+			szTemp, ntohs(pClient->connectAddr.sin_port), pClient->dwUserNO);
+	}
+}
+
+st_CLIENT * FindClient(DWORD dwUserNO)
+{
+
+
+}
+
+BOOL DisconnectClient(DWORD dwUserNO)
+{
+	DWORD dwEnterRoomNO = g_ClientMap[dwUserNO]->dwEnterRoomNO;
+
+	// 방에 들어가 있다면
+	// 그 방 유저들에게 broadcast
+
+	// 동적할당한 구조체 삭제
+	delete g_ClientMap[dwUserNO];
+
+	// 맵에서 삭제
+	g_ClientMap.erase(dwUserNO);
+
+	return TRUE;
+}
+
+BOOL netPacket_ReqLogin(st_CLIENT * pClient, CProtocolBuffer * pPacket)
+{
+	return 0;
+}
+
+BOOL netPacket_ReqRoomList(st_CLIENT * pClient, CProtocolBuffer * pPacket)
+{
+	return 0;
+}
+
+BOOL netPacket_ReqRoomCreate(st_CLIENT * pClient, CProtocolBuffer * pPacket)
+{
+	WCHAR	szRoomTitle[256] = { 0, };
+	WORD	wTitleSize;
+
+	(*pPacket) >> wTitleSize;
+
+	if ( 256 < wTitleSize )
+	{
+		wTitleSize = 255;
+	}
+
+	pPacket->GetData( (char*)szRoomTitle, wTitleSize );
+
+	// 방 생성
+	st_CHAT_ROOM *pRoom = new st_CHAT_ROOM;
+	memset( pRoom->szTitle, 0, sizeof(WCHAR) * 256 );
+
+	pRoom->dwRoomNO = g_dwKey_RoomNO++;
+
+	wcscpy_s( pRoom->szTitle, szRoomTitle );
+
+	g_RoomMap.insert( pair<DWORD, st_CHAT_ROOM*>(pRoom->dwRoomNO, pRoom) );
+
+	// 방생성 로그
+	wprintf( L"방 생성 [UserNO:%d][Room:%s][TotalRoom:%d]\n",
+		pClient->dwUserNO, pRoom->szTitle, g_RoomMap.size() );
+
+	// 방 생성 결과를 클라로 전송
+	Send_ResRoomCreate( pClient, df_RESULT_ROOM_CREATE_OK, pRoom );
+
+	return TRUE;
+}
+
+BOOL netPacket_ReqRoomEnter(st_CLIENT * pClient, CProtocolBuffer * pPacket)
+{
+
+	return 0;
+}
+
+BOOL netPacket_ReqChat(st_CLIENT * pClient, CProtocolBuffer * pPacket)
+{
+	return 0;
+}
+
+BOOL netPacket_ReqRoomLeave(st_CLIENT * pClient, CProtocolBuffer * pPacket)
+{
+	return 0;
+}
+
+void Send_ResLogin(st_CLIENT * pClient, BYTE byResult)
+{
+}
+
+void Send_ResRoomList(st_CLIENT * pClient, BYTE byResult)
+{
+}
+
+void Send_ResRoomCreate(st_CLIENT * pClient, st_CHAT_ROOM * pRoom, BYTE byResult)
+{
+}
+
+void Send_ResRoomEnter(st_CLIENT * pClient, st_CHAT_ROOM * pRoom, BYTE byResult)
+{
+}
+
+void Send_ResChat(st_CLIENT * pClient, WORD wMsgSize, PWCHAR szMsg)
+{
+}
+
+void Send_ResRoomLeave(st_CLIENT * pClient)
+{
+}
+
+void Send_ResRoomDelete(st_CLIENT * pClient, st_CHAT_ROOM * pRoom)
+{
+}
+
+void Send_ResUserEnter(st_CLIENT * pClient)
+{
+}
+
+void Send_ResRoomCreate(st_CLIENT * pClient, BYTE byResult, st_CHAT_ROOM * pRoom)
+{
+	st_PACKET_HEADER stHeader;
+	CProtocolBuffer	clPacket;
+
+	MakePacket_ResRoomCreate(&stHeader, &clPacket, byResult, pRoom);
+
+	// 채팅방 생성이 성공인 경우 모든 유저에게
+	if ( df_RESULT_ROOM_CREATE_OK == byResult)
+	{
+		SendBroadcast(&stHeader, &clPacket);
+	}
+	// 실패면 방 만들기 요청한 유저에게만
+	else
+	{
+		SendUnicast(pClient, &stHeader, &clPacket);
+	}
+}
+
+void MakePacket_ResLogin(st_PACKET_HEADER * pHeader, CProtocolBuffer * pPacket, BYTE byResult, st_CLIENT * pClient)
+{
+}
+
+void MakePacket_ResRoomList(st_PACKET_HEADER * pHeader, CProtocolBuffer * pPacket, BYTE byResult)
+{
+}
+
+void MakePacket_ResRoomCreate(st_PACKET_HEADER *pHeader, CProtocolBuffer * pPacket, BYTE byResult, st_CHAT_ROOM * pRoom)
+{
+	WORD	wTitleSize;
+
+	pPacket->Clear();
+	pPacket->PutData((char*)&byResult, sizeof(BYTE));
+
+	if (byResult == df_RESULT_ROOM_CREATE_OK)
+	{
+		*pPacket << pRoom->dwRoomNO;
+		wTitleSize = wcslen(pRoom->szTitle) * sizeof(WCHAR);
+		*pPacket << wTitleSize;
+		pPacket->PutData( (char*)pRoom->szTitle, wTitleSize );
+	}
+
+	pHeader->byCode			= dfPACKET_CODE;
+	pHeader->byCheckSum		= MakeCheckSum(pPacket, df_RES_ROOM_CREATE);
+	pHeader->wMsgType		= df_RES_ROOM_CREATE;
+	pHeader->wPayloadSize	= pPacket->GetDataSize();
+}
+
+void MakePacket_ResRoomEnter(st_PACKET_HEADER * pHeader, CProtocolBuffer * pPacket, BYTE byResult, st_CHAT_ROOM * pRoom)
+{
+}
+
+void MakePacket_ResChat(st_PACKET_HEADER * pHeader, CProtocolBuffer * pPacket, st_CLIENT * pClient, BYTE byResult, WORD wMsgSize, PWCHAR szMsg)
+{
+}
+
+void MakePacket_ResUserEnter(st_PACKET_HEADER * pHeader, CProtocolBuffer * pPacket, st_CLIENT * pClient)
+{
+}
+
+void MakePacket_ResRoomLeave(st_PACKET_HEADER * pHeader, CProtocolBuffer * pPacket, st_CLIENT * pClient)
+{
+}
+
+void MakePacket_ResRoomDelete(st_PACKET_HEADER * pHeader, CProtocolBuffer * pPacket, st_CLIENT * pClient, st_CHAT_ROOM * pRoom)
+{
+}
+
+void SendUnicast(st_CLIENT * pClient, st_PACKET_HEADER * pHeader, CProtocolBuffer * pPacket)
+{
+	if (nullptr == pClient)
+	{
+		err_display(L"SendUnicast()");
+		return;
+	}
+
+	pClient->SendQ.Put( (char *)pHeader, sizeof(st_PACKET_HEADER) );
+	pClient->SendQ.Put( (char *)pPacket->GetBufferPtr(), pPacket->GetDataSize() );
+}
+
+void SendBroadcast(st_PACKET_HEADER * pHeader, CProtocolBuffer * pPacket)
+{
+	st_CLIENT *pClient;
+
+	map<DWORD, st_CLIENT*>::iterator iter;
+
+	for (iter = g_ClientMap.begin(); iter != g_ClientMap.end(); iter++)
+	{
+		pClient = iter->second;
+
+		SendUnicast(pClient, pHeader, pPacket);
+	}
+}
+
+void SendBroadcastRoom(st_CHAT_ROOM * pRoom, st_CLIENT * pClient, st_PACKET_HEADER * pHeader, CProtocolBuffer * pPacket)
+{
+	st_CLIENT *pDestClient;
+
+	list<DWORD>::iterator iter;
+
+	for ( iter = pRoom->UserList.begin(); iter != pRoom->UserList.end(); iter++ )
+	{
+		pDestClient = g_ClientMap[*iter];
+
+		if ( pDestClient != pClient )
+		{
+			SendUnicast( pDestClient, pHeader, pPacket );
+		}
+	}
+}
+
+
